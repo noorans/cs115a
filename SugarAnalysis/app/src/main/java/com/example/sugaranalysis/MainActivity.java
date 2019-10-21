7992dd171247c2eecb0d8c75112a5edbf6721d7a
noorans <salimnooran897@gmail.com>
2019-10-21 00:57:12 -0700

    Adding times

--- SugarAnalysis/app/src/main/java/com/example/sugaranalysis/MainActivity.java
+++ SugarAnalysis/app/src/main/java/com/example/sugaranalysis/MainActivity.java
@@ -9,17 +9,40 @@ import android.os.Bundle;
 //import android.view.MenuItem;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView progressButton, bmiButton, alertsButton, contactsButton, settingsButton;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listview);
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Before Breakfast");
+        arrayList.add("After Breakfast");
+        arrayList.add("Before Lunch");
+        arrayList.add("After Lunch");
+        arrayList.add("Before Dinner");
+        arrayList.add("After Dinner");
+        arrayList.add("Before Bedtime Snack");
+        arrayList.add("Before Workout");
+        arrayList.add("After Workout");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+
 
         progressButton = findViewById(R.id.progressIcon);
         bmiButton = findViewById(R.id.bmiIcon);
