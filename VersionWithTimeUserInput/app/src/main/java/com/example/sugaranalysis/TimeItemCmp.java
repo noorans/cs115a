626fe5293cf3c02018a4a5e7913edccd0a19a006
pranavkn <Pranav@eduroam-169-233-226-166.ucsc.edu>
2019-11-21 21:01:16 -0800

    User Input for time. This version of the app has no compile or runtime errors, but crashes on run.

---
+++ VersionWithTimeUserInput/app/src/main/java/com/example/sugaranalysis/TimeItemCmp.java
@@ -0,0 +1,25 @@
+package com.example.sugaranalysis;
+
+public class TimeItemCmp {
+
+    private String userTime;
+    private String title;
+
+    //using getter and setter methods
+    public String getEditTextValue() {
+        return userTime;
+    }
+
+    public void setEditTextValue(String userTime) {
+        this.userTime = userTime;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
