4429ee6db32e097581d002c12384bad13f1f6bc0
Nicole Ng <nmng@ucsc.edu>
2019-11-25 11:55:12 -0800

    working

--- app/src/main/java/com/example/sugaranalysis/AlertsActivity.java
+++ app/src/main/java/com/example/sugaranalysis/AlertsActivity.java
@@ -5,11 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import com.allyants.notifyme.NotifyMe;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
-public class AlertsActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AlertsActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,
+        TimePickerDialog.OnTimeSetListener {
     ImageView progressButton, bmiButton, alertsButton, extrasButton,
             settingsButton, logsButton, homeButton;
+    TimePickerDialog timePickerDialog;
+    DatePickerDialog datePickerDialog;
+    EditText meal, description;
+    Button notifyButton;
+    Calendar now = Calendar.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,25 @@ public class AlertsActivity extends AppCompatActivity {
         extrasButton = findViewById(R.id.extra);
         settingsButton = findViewById(R.id.settings);
         logsButton = findViewById(R.id.logs);
+
+        meal = findViewById(R.id.meal);
+        description = findViewById(R.id.description);
+        notifyButton = findViewById(R.id.notifyButton);
+
+        datePickerDialog = DatePickerDialog.newInstance(
+                AlertsActivity.this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+
+        timePickerDialog = TimePickerDialog.newInstance(
+                AlertsActivity.this,
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                now.get(Calendar.SECOND),
+                false
+        );
 
         // start new activity to view settings activity
         settingsButton.setOnClickListener(new View.OnClickListener() {
@@ -41,7 +74,45 @@ public class AlertsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        notifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog.show(getFragmentManager(), "Date Picker Dialog");
+            }
+        });
     }
+
+    // SET DATE
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        now.set(Calendar.YEAR,year);
+        now.set(Calendar.MONTH,monthOfYear);
+        now.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        timePickerDialog.show(getFragmentManager(), "Time Picker Dialog");
+    }
+
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+        now.set(Calendar.HOUR_OF_DAY,hourOfDay);
+        now.set(Calendar.MINUTE,minute);
+        now.set(Calendar.SECOND,second);
+        Intent intent = new Intent(getApplicationContext(),AlertsActivity.class);
+        intent.putExtra("test","I am a String");
+        NotifyMe notifyMe = new NotifyMe.Builder(getApplicationContext())
+                .title(meal.getText().toString())
+                .content(description.getText().toString())
+                .color(255,0,0,255)
+                .led_color(255,255,255,255)
+                .time(now)
+                .addAction(intent,"Snooze",false)
+                .key("test")
+                .addAction(new Intent(),"Dismiss",true,false)
+                .addAction(intent,"Done")
+                .large_icon(R.mipmap.ic_launcher_round)
+                .build();
+    }
+
     // start new activity to view progress activity
     public void clickProgress(View v) {
         Intent intent = new Intent(AlertsActivity.this, ProgressActivity.class);
