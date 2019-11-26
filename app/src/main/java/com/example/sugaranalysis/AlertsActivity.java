a6729730a325fe39fdd9bf6d88ab3cac5f98f4cc
Nicole Ng <nmng@ucsc.edu>
2019-11-26 00:05:01 -0800

    military time 'works'

--- app/src/main/java/com/example/sugaranalysis/AlertsActivity.java
+++ app/src/main/java/com/example/sugaranalysis/AlertsActivity.java
@@ -8,21 +8,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.allyants.notifyme.NotifyMe;
 
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class AlertsActivity extends AppCompatActivity  {
     ImageView progressButton, bmiButton, alertsButton, extrasButton,
             settingsButton, logsButton, homeButton;
+    ImageButton bb, ab, bl, al, bd, ad, bw, aw;
     EditText label, time;
     Button notifyButton;
-    String labels, times;
+    String labels, times, setLabel;
+    int count  = 0;
     Calendar now = Calendar.getInstance();
 
 
@@ -40,11 +47,16 @@ public class AlertsActivity extends AppCompatActivity  {
         settingsButton = findViewById(R.id.settings);
         logsButton = findViewById(R.id.logs);
 
-        label = findViewById(R.id.label);
+        //label = findViewById(R.id.label);
         time = findViewById(R.id.time);
-        notifyButton = findViewById(R.id.notifyButton);
-
-
+        bb = findViewById(R.id.bb);
+        ab = findViewById(R.id.ab);
+        bl = findViewById(R.id.bl);
+        al = findViewById(R.id.al);
+        bd = findViewById(R.id.bd);
+        ad = findViewById(R.id.ad);
+        bw = findViewById(R.id.bw);
+        aw = findViewById(R.id.aw);
 
 
         // start new activity to view settings activity
@@ -65,19 +77,102 @@ public class AlertsActivity extends AppCompatActivity  {
             }
         });
 
-        notifyButton.setOnClickListener(new View.OnClickListener() {
+
+
+        //parseForTime(times);
+
+        bb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                labels = label.getText().toString();
+                bb.setBackgroundResource(R.drawable.bb_filled);
                 times = time.getText().toString();
-                parseForTime(labels, times);
-
+                labels = "Before Breakfast";
+                parseForTime(times);
+                Toast.makeText(AlertsActivity.this, "SET TIME BEFORE BREAKFAST", Toast.LENGTH_LONG).show();
             }
         });
+
+        ab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ab.setBackgroundResource(R.drawable.afb_filled);
+                times = time.getText().toString();
+                labels = "After Breakfast";
+                parseForTime(times);
+                Toast.makeText(AlertsActivity.this, "SET TIME AFTER BREAKFAST", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        bl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bl.setBackgroundResource(R.drawable.bl_filled);
+                times = time.getText().toString();
+                labels = "Before Lunch";
+                parseForTime(times);
+                Toast.makeText(AlertsActivity.this, "SET TIME BEFORE LUNCH", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        al.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                al.setBackgroundResource(R.drawable.al_filled);
+                times = time.getText().toString();
+                labels = "After Lunch";
+                parseForTime(times);
+                Toast.makeText(AlertsActivity.this, "SET TIME AFTER LUNCH", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        bd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bd.setBackgroundResource(R.drawable.bd_filled);
+                times = time.getText().toString();
+                labels = "Before Dinner";
+                parseForTime(times);
+                Toast.makeText(AlertsActivity.this, "SET TIME BEFORE DINNER", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ad.setBackgroundResource(R.drawable.ad_filled);
+                times = time.getText().toString();
+                labels = "After Dinner";
+                parseForTime(times);
+                Toast.makeText(AlertsActivity.this, "SET TIME AFTER DINNER", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        bw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bw.setBackgroundResource(R.drawable.bw_filled);
+                times = time.getText().toString();
+                labels = "Before Workout";
+                parseForTime(times);
+                Toast.makeText(AlertsActivity.this, "SET TIME BEFORE WORKOUT", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        aw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aw.setBackgroundResource(R.drawable.aw_filled);
+                times = time.getText().toString();
+                labels = "After Workout";
+                parseForTime(times);
+                Toast.makeText(AlertsActivity.this, "SET TIME AFTER WORKOUT", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
-    public void parseForTime(String labels, String times) {
-        /*SimpleDateFormat time = new SimpleDateFormat("hh:mm aa");
+    public void parseForTime(String times) {
+        SimpleDateFormat time = new SimpleDateFormat("HH:mm");
         //String [] parts = times.split(" ");
         //String am_or_pm = parts[1];
         Calendar now = Calendar.getInstance();
@@ -86,31 +181,26 @@ public class AlertsActivity extends AppCompatActivity  {
         try {
            date = time.parse(times);
 
-
         }
         catch (ParseException e) {
             e.printStackTrace();
         }
         int hours = date.getHours();
         int mins =  date.getMinutes();
-        //now.set(Calendar.HOUR, Calendar.HOUR);
-        //now.set(Calendar.MINUTE, Calendar.MINUTE);
-        /*if(am_or_pm.equals("AM")) {
-            now.set(Calendar.AM_PM, 0);
-        }
-        else if (am_or_pm.equals("PM")){
-            now.set(Calendar.AM_PM, 1);
-        }*/
+
+        now.set(Calendar.HOUR, hours);
+        now.set(Calendar.MINUTE, mins);
+
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.putExtra("test","I am a String");
         NotifyMe notifyMe = new NotifyMe.Builder(getApplicationContext())
-                .title("Time To Log Blood Sugar")
+                .title(labels)
                 .color(255,0,0,255)
                 .led_color(255,255,255,255)
                 .addAction(intent,"Snooze",false)
                 .key("test")
-                //.time()
+                .time(now)
                 .addAction(new Intent(),"Dismiss",true,false)
                 .addAction(intent, "Home")
                 .large_icon(R.mipmap.ic_launcher_round)
